6b73a29dccfdfba69102644aa25c46e72c005f94
abilashravi-ta <abilash.ravi@tigeranalytics.com>
2024-03-14 14:02:15 +0530

    added docstrings

--- simple_functions.py
+++ simple_functions.py
@@ -1,8 +1,10 @@
 # Custom python functions
 
 def double_number(a):
+    """A function to double a given number."""
     return a+a
 
 def square_number(a):
+    """A function to square a given number."""
     return a*a
 
