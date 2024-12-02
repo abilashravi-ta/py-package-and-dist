ab241f11cc5b5db29588abbbc16f66fae186ab60
abilashravi-ta <abilash.ravi@tigeranalytics.com>
2024-12-02 11:16:46 +0530

    Created django-std-tables-creation.sql file

---
+++ django-std-tables-creation.sql
@@ -0,0 +1,44 @@
+/****** auth_user table creation ******/
+CREATE TABLE [dbo].[auth_user](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[password] [nvarchar](128) NOT NULL,
+	[last_login] [datetimeoffset](7) NULL,
+	[is_superuser] [bit] NOT NULL,
+	[username] [nvarchar](150) NOT NULL,
+	[first_name] [nvarchar](150) NOT NULL,
+	[last_name] [nvarchar](150) NOT NULL,
+	[email] [nvarchar](254) NOT NULL,
+	[is_staff] [bit] NOT NULL,
+	[is_active] [bit] NOT NULL,
+	[date_joined] [datetimeoffset](7) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [auth_user_username_6821ab7c_uniq] UNIQUE NONCLUSTERED 
+(
+	[username] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+/****** django_session table creation ******/
+CREATE TABLE [dbo].[django_session](
+	[session_key] [nvarchar](40) NOT NULL,
+	[session_data] [nvarchar](max) NOT NULL,
+	[expire_date] [datetimeoffset](7) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[session_key] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** django_session table index creation ******/
+CREATE NONCLUSTERED INDEX [django_session_expire_date_a5c62663] ON [dbo].[django_session]
+(
+	[expire_date] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+GO
